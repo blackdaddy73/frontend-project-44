7294c32e994983dd249a2492e7e18a8ffb7ea267
Ildar Asanov <air1983@yandex.ru>
2024-07-26 20:32:33 +0300

    fix js errors add brain-gcd file

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import readlineSync from 'readline-sync';
 const name = welcome();
 import brainEven from '../bin/games/brain-even.js';
 import brainCalc from '../bin/games/brain-calc.js';
+import brainGCD from '../bin/games/brain-gcd.js';
 //import gameSelection from '../utilities/game-selection.js';
-let selection = Math.round(Math.random(1) * 1) + 1; //не более 2 вариантов, позже сделаем выбор макс вариантов
+let selection = Math.round(Math.random(1) * 2) + 1; //не более 3 вариантов, позже сделаем выбор макс вариантов
 let game = 1;
     switch (selection) {
         case 1: game = brainEven;
         break;
         case 2: game = brainCalc;
         break;
+        case 3: game = brainGCD;
     };
 let NumberOfAnswers = 0;
 for ( ; NumberOfAnswers !== 3; ) {

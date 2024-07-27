e2b0949aaed09304d16cc900fdb1df2b5dcb8e79
Ildar Asanov <air1983@yandex.ru>
2024-07-27 22:04:13 +0300

    add all games edit index.js

--- src/index.js
+++ src/index.js
@@ -5,16 +5,23 @@ const name = welcome();
 import brainEven from '../bin/games/brain-even.js';
 import brainCalc from '../bin/games/brain-calc.js';
 import brainGCD from '../bin/games/brain-gcd.js';
+import brainProgression from '../bin/games/brain-progression.js';
+import brainPrime from '../bin/games/brain-prime.js';
 //import gameSelection from '../utilities/game-selection.js';
-let selection = Math.round(Math.random(1) * 2) + 1; //не более 3 вариантов, позже сделаем выбор макс вариантов
+let selection = Math.round(Math.random(1) * 4) + 1; //не более 5 вариантов, позже сделаем выбор макс вариантов
 let game = 1;
-selection = 3; //Для тестов временно
+selection = 5; //Для тестов временно
     switch (selection) {
         case 1: game = brainEven;
         break;
         case 2: game = brainCalc;
         break;
         case 3: game = brainGCD;
+        break;
+        case 4: game = brainProgression;
+        break;
+        case 5: game = brainPrime;
+        break;
     };
 let NumberOfAnswers = 0;
 for ( ; NumberOfAnswers !== 3; ) {

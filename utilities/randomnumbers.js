7d100fedd809a4e8fd19f762816401d4e5698515
Ildar Asanov <air1983@yandex.ru>
2024-07-30 20:09:00 +0300

    del files

--- utilities/randomnumbers.js
+++
@@ -1,7 +0,0 @@
-const getRandomNumbers = () => {
-    const randomNumbers = [];
-    randomNumbers[0] = Math.round(Math.random(1) * 99) + 1;
-    randomNumbers[1] = Math.round(Math.random(1) * 99) + 1;
-    return randomNumbers;
-};
-export default getRandomNumbers;

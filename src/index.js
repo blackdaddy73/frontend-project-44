73cff530e700912333ee252340c482cbef14853d
Ildar Asanov <air1983@yandex.ru>
2024-08-14 15:56:01 +0300

    last fix index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import welcome from './cli.js';
 const startAllGames = (description, getGameData) => {
   const name = welcome();
   console.log(description);
-  let rounds = 0;
-  for (; rounds !== 3;) {
+  for (let i = 0; i !== 3; i += 1) {
     const gameData = getGameData();
     const question = gameData[0];
     const correctAnswer = gameData[1];
@@ -13,14 +12,11 @@ const startAllGames = (description, getGameData) => {
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer === correctAnswer) {
       console.log('Correct!');
-      rounds += 1;
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'\nLet's try again, ${name}!`);
       return;
     }
   }
-  if (rounds === 3) {
     console.log(`Congratulations, ${name}!`);
-  }
 };
 export default startAllGames;
